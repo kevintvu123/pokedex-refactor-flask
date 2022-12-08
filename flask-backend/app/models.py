61e567f2e741e54b8678304666ef95c5866e87f8
Kevin <kevintvu123@gmail.com>
2022-12-08 16:06:18 -0500

    Read All Pokemon

--- flask-backend/app/models.py
+++ flask-backend/app/models.py
@@ -40,8 +40,8 @@ class Pokemon(db.Model):
         db.Float(precision=3, decimal_return_scale=2), nullable=False, default=1.00
     )
     captured = db.Column(db.Boolean, default=False, nullable=False)
-    items = db.relationship("Item", back_populates='pokemon')
-    
+    items = db.relationship("Item", back_populates="pokemon")
+
 
 class Item(db.Model):
     __tablename__ = "items"
@@ -50,6 +50,15 @@ class Item(db.Model):
     image_url = db.Column(db.String(255), nullable=False)
     name = db.Column(db.String(255), nullable=False)
     price = db.Column(db.Integer, nullable=False)
-    pokemon_id = db.Column(db.Integer, db.ForeignKey('pokemons.id'))
-    pokemon = db.relationship("Pokemon", back_populates='items')
-    
+    pokemon_id = db.Column(db.Integer, db.ForeignKey("pokemons.id"))
+    pokemon = db.relationship("Pokemon", back_populates="items")
+
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            happiness=self.happiness,
+            image_url=self.image_url,
+            name=self.name,
+            price=self.price,
+            pokemon_id=self.pokemon_id,
+        )
--- flask-backend/app/routes/pokemon.py
+++ flask-backend/app/routes/pokemon.py
@@ -1,33 +1,36 @@
 from flask import Blueprint, jsonify
 from ..models import Pokemon, db, Type, Item
-import enum
 
 
-bp = Blueprint("pokemon", __name__, url_prefix='/api/pokemon')
+bp = Blueprint("pokemon", __name__, url_prefix="/api")
 
 
-@bp.route("/")
+@bp.route("/pokemon")
 def index():
     all_pokemon = Pokemon.query.all()
-    
+    # print(all_pokemon)
     pokemon = []
     for p in all_pokemon:
-        print(p.type)
-        pokemon.append({
-            "id": p.id,
-            "number": p.number,
-            "attack": p.attack,
-            "defense": p.defense,
-            "image_url": p.image_url,
-            "name": p.name,
-            # "type": p.type,
-            "moves": p.moves,
-            "encounter_rate": p.encounter_rate,
-            "catch_rate": p.catch_rate,
-            "captured": p.captured,
-            # "items": p.items
-        })
+        # print(p.type.value)
+        # print((p.items))
+        items = [item.to_dict() for item in p.items]
+        pokemon.append(
+            {
+                "id": p.id,
+                "number": p.number,
+                "attack": p.attack,
+                "defense": p.defense,
+                "image_url": p.image_url,
+                "name": p.name,
+                "type": p.type.value,
+                "moves": p.moves,
+                "encounter_rate": p.encounter_rate,
+                "catch_rate": p.catch_rate,
+                "captured": p.captured,
+                "items": items,
+            }
+        )
 
-    print("POKEMON LSIT OF OBJECTS",pokemon)
-        
-    return jsonify(pokemon)
+    # print("POKEMON LSIT OF OBJECTS",pokemon)
+
+    return pokemon
